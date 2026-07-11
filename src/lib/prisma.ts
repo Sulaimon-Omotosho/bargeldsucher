0861e1605f4f4d39df489c547d81b3c1e3f9c74d
Sulaimon Omotosho <susudef2u@gmail.com>
2026-07-11 19:04:42 +0100

    BugFix: Fixing prisma bug

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,5 @@
-// import { PrismaClient } from '@/generated/prisma/client'
-
-// const globalForPrisma = globalThis as typeof globalThis & {
-//   prisma?: PrismaClient
-// }
-
-// export const db = globalForPrisma.prisma ?? new PrismaClient()
-
-// if (process.env.NODE_ENV !== 'production') {
-//   globalForPrisma.prisma = db
-// }
-import { PrismaClient } from '@/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../../generated/prisma/client'
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
